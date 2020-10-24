425f1ea5da6f5f7c90bec982add477f2713f6928
Ram4Gamer <ram4gamer9@gmail.com>
2020-10-24 15:11:29 +0200

    Queue changes

--- src/main/java/com/solardev/SolarHub/queue/Queue.java
+++ src/main/java/com/solardev/SolarHub/queue/Queue.java
@@ -1,10 +1,14 @@
 package com.solardev.SolarHub.queue;
 
 import com.solardev.SolarHub.SolarHub;
+import com.solardev.SolarHub.utils.BungeeUtils;
+import com.solardev.SolarHub.utils.QueueUtils;
 import com.solardev.SolarHub.utils.chat.Color;
+import com.solardev.SolarHub.utils.runnable.Runnable;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitRunnable;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -49,5 +53,52 @@ public class Queue {
         }else {
             return;
         }
+    }
+
+    public void startQueue(Player player, String server) {
+        List<Player> queue1 = QueueUtils.queue1;
+        Bukkit.getScheduler().scheduleSyncDelayedTask(SolarHub.getINSTANCE(), new java.lang.Runnable() {
+            @Override
+            public void run() {
+                if (queue1.get(0).getPlayer() != null) {
+                    Player pos1 = queue1.get(0);
+                    String sendingMessage = plugin.getLang().getString("send-to-server").replaceAll("<server>", server);
+                    pos1.sendMessage(Color.translate(sendingMessage));
+                    queue1.remove(pos1);
+                    plugin.setMainScoreboard(pos1);
+                    BungeeUtils.send(pos1, plugin.getConfig().getString("servers." + server + ".bungeecord-name"));
+                }
+            }
+        }, plugin.getConfig().getLong("queue.delay"));
+    }
+
+    public void sendMessage(Player player, String server) {
+        List<Player> queue1 = QueueUtils.queue1;
+        int urPos = queue1.lastIndexOf(player) + 1;
+        Runnable.scheduleSyncDelayedTask(SolarHub.getINSTANCE(), new BukkitRunnable() {
+            @Override
+            public void run() {
+                if (!queue1.contains(player)) {
+                    Bukkit.getScheduler().cancelTask(this.getTaskId());
+                }else {
+                    if (urPos == 0 && queue1.size() == 1) {
+                        queue1.remove(player);
+                        Bukkit.getScheduler().cancelTask(this.getTaskId());
+                    }else {
+                        if (!(queue1.lastIndexOf(player) == 0) && queue1.size() == 0) {
+                            queue1.remove(player);
+                            Bukkit.getScheduler().cancelTask(this.getTaskId());
+                        }else {
+                            int urPos1 = urPos - 1;
+                            if (queue1.lastIndexOf(player) == 0) {
+                                player.sendMessage(Color.translate(plugin.getConfig().getString("queue.message").replaceAll("<pos>", String.valueOf(urPos) + "#").replaceAll("<total>", queue1.size() + "#")));
+                            }else {
+                                player.sendMessage(Color.translate(plugin.getConfig().getString("queue.message").replaceAll("<pos>", urPos + "#").replaceAll("<total>", String.valueOf(queue1.size()) + "#")));
+                            }
+                        }
+                    }
+                }
+            }
+        }, plugin.getConfig().getLong("queue.delay"));
     }
 }
